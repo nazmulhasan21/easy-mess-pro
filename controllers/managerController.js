7b7bc028c465ec16b0acd9a2dad99fb477d263ac
nazmulhasan21 <nazmulhasen892@gmail.com>
2022-08-27 12:06:29 +0600

    add sub manager  and delete sub manager and change mess manager route and controller fixed

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -14,7 +14,7 @@ exports.changeManager = async (req, res, next) => {
   try {
     const { user } = req;
     const { userId } = req.params.userId;
-    const isValid = mongoose.Types.ObjectId.isValid(userId);
+    const isValid = mongoose.Types.ObjectId.isValid(req.params.userId);
     if (!isValid)
       return next(new AppError(400, 'userId', 'userId is not valid '));
     //  1 check your mess and already exit this user in mess manager
