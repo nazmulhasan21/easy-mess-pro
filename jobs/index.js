58f9b948c079795849f3f7b13b5bc4e758af50ff
nazmulhasan21 <nazmulhasen892@gmail.com>
2023-02-23 02:18:05 +0600

    set job schedule for auto meal update

--- jobs/index.js
+++ jobs/index.js
@@ -6,6 +6,6 @@ schedule.scheduleJob(`00     10   2    *    *    *`, async () => {
   // update every mess active month
   await updateMeal();
 });
-schedule.scheduleJob(`00     17   02    *    *    *`, async () => {
+schedule.scheduleJob(`00    25   02    *    *    *`, async () => {
   console.log('scheule');
 });
