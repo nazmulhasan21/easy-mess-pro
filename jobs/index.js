faee50540e91ba10c4a7aa1b6e93939946ece4ad
nazmulhasan21 <nazmulhasen892@gmail.com>
2023-02-23 02:48:15 +0600

    set job schedule for

--- jobs/index.js
+++ jobs/index.js
@@ -2,10 +2,7 @@ const schedule = require('node-schedule');
 const { updateMeal } = require('./updateManager');
 
 // schedule jobs
-schedule.scheduleJob(`00     10   2    *    *    *`, async () => {
+schedule.scheduleJob(`00     15   10    *    *    *`, async () => {
   // update every mess active month
   await updateMeal();
-});
-schedule.scheduleJob(`00    25   02    *    *    *`, async () => {
-  console.log('scheule');
 });
