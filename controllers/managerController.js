3562dac4986b7c12ceb4533d8ede6e3a5931514b
nazmulhasan21 <nazmulhasen892@gmail.com>
2023-07-04 17:34:07 +0600

    change manager bug fix

--- controllers/managerController.js
+++ controllers/managerController.js
@@ -34,67 +34,69 @@ exports.changeManager = async (req, res, next) => {
         )
       );
 
-    //  1 check your mess and already exit this user in mess manager
-    const manager = await Mess.findOne({
-      $and: [{ _id: user.messId }, { manager: userId }],
-    }).select('manager');
-    if (manager)
-      return next(
-        new AppError(
-          403,
-          'manager',
-          'এই ব্যাক্তি ইতিমধ্যে এই মেস ম্যানেজার, অন্য ব্যাক্তি নির্বাচন করুন'
-        )
-      );
+    // //  1 check your mess and already exit this user in mess manager
+    // const manager = await Mess.findOne({
+    //   $and: [{ _id: user.messId }, { manager: userId }],
+    // }).select('manager');
+    // if (manager)
+    //   return next(
+    //     new AppError(
+    //       403,
+    //       'manager',
+    //       'এই ব্যাক্তি ইতিমধ্যে এই মেস ম্যানেজার, অন্য ব্যাক্তি নির্বাচন করুন'
+    //     )
+    //   );
 
-    const mess = await Mess.findById(user.messId).select('manager');
+    const mess = await Mess.findById(user.messId);
 
-    // update before manager role border
-    const change = await User.findByIdAndUpdate(
-      mess.manager,
-      {
-        role: 'border',
-      },
-      {
-        new: true,
-        runValidators: true,
-      }
+    // // update before manager role border
+    await User.updateMany({ messId: user.messId }, { role: 'border' });
+    // const change = await User.findByIdAndUpdate(
+    //   mess.manager,
+    //   {
+    //     role: 'border',
+    //   },
+    //   {
+    //     new: true,
+    //     runValidators: true,
+    //   }
+    // );
+
+    // if (change.role == 'border') {
+    // 2 find user and change this user role manager
+    const newManager = await User.findOneAndUpdate(
+      { $and: [{ _id: userId }, { messId: mess._id }] },
+      { role: 'manager' }
     );
+    // update mess manager
+    mess.manager = newManager._id;
+    // active month manager change
+    await mess.save();
 
-    if (change.role == 'border') {
-      // 2 find user and change this user role manager
-      const newManager = await User.findOneAndUpdate(
-        { $and: [{ _id: userId }, { messId: mess._id }] },
-        { role: 'manager' }
-      );
-      // update mess manager
-      mess.manager = newManager._id;
-      // active month manager change
-
-      // Push Notifications with Firebase
-      const pushTitle = 'আপনার মেস ম্যানেজার পরিবর্তন হয়েছে';
-      const pushBody = `${newManager.name} কে আপনার মেস ম্যানেজার করা হলো`;
-      const FCMTokens = await getMessMemberFCMTokens(user.messId);
-      if (FCMTokens) {
-        await pushNotificationMultiple(pushTitle, pushBody, FCMTokens);
-      }
-
-      // await Notification.create({
-      //   messId: user.messId,
-      //   user: userId,
-      //   title: pushTitle,
-      //   description: pushBody,
-      //   date: month.updatedAt,
-      // });
-
-      // send response
-      res.status(201).json({
-        status: 'success',
-        message: 'সফলভাবে আপনার মেস ম্যানেজার পরিবর্তন হয়েছে।',
-      });
-    } else {
-      return next(new AppError(402, 'error', 'অনুগ্রহ করে আবার চেষ্টা করুন।'));
+    // Push Notifications with Firebase
+    const pushTitle = 'আপনার মেস ম্যানেজার পরিবর্তন হয়েছে';
+    const pushBody = `${newManager.name} কে আপনার মেস ম্যানেজার করা হলো`;
+    const FCMTokens = await getMessMemberFCMTokens(user.messId);
+    if (FCMTokens) {
+      await pushNotificationMultiple(pushTitle, pushBody, FCMTokens);
     }
+
+    // await Notification.create({
+    //   messId: user.messId,
+    //   user: userId,
+    //   title: pushTitle,
+    //   description: pushBody,
+    //   date: month.updatedAt,
+    // });
+
+    // send response
+    res.status(201).json({
+      status: 'success',
+      message: 'সফলভাবে আপনার মেস ম্যানেজার পরিবর্তন হয়েছে।',
+    });
+    // } else {
+    //   return next(new AppError(402, 'error', 'অনুগ্রহ করে আবার চেষ্টা করুন।'));
+    // }
   } catch (error) {
     next(error);
   }
